fbf6801f0f592402d8c46d18b37947f228d788c6
Juan David <juann0@outlook.com>
2024-03-16 01:53:38 -0500

    Segunda subida

--- src/routes/inicio.js
+++ src/routes/inicio.js
@@ -5,4 +5,8 @@ router.get('/', async (req, res) => {
     res.render('./app/index', { estilosLogin: true, titulo: 'Inicio de sesión' });
 });
 
+router.get('/reset', async (req, res) => {
+    res.render('./app/reset', { estilosLogin: true, titulo: 'Olvide contraseña' });
+});
+
 module.exports = router;
